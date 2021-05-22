8007339c1a9b24d71bb16d4b507b6d979e1c72eb
nerfsunny <nerfsunny@gmail.com>
2021-05-21 23:33:08 -0400

    Current progress on problem. Currently passes 7/11 tests. Getting 'Time Limit Exceeded' on remaining tests.

---
+++ The Great XOR/thegreatxor.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'theGreatXor' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER x as parameter.
+ */
+
+long theGreatXor(long x) {
+    long count = 0, xorValue;
+    
+    for(long i = 1; i < x; i++) {
+        xorValue = i ^ x;
+        
+        if(xorValue > x)
+            count++;
+    }
+    
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string q_temp;
+    getline(cin, q_temp);
+
+    int q = stoi(ltrim(rtrim(q_temp)));
+
+    for (int q_itr = 0; q_itr < q; q_itr++) {
+        string x_temp;
+        getline(cin, x_temp);
+
+        long x = stol(ltrim(rtrim(x_temp)));
+
+        long result = theGreatXor(x);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
